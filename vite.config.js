131631d9ba9eff0c73573919862af1d6dcce6f97
AfsalKabeerU <afsalkabeer4u@gmail.com>
2025-06-21 14:48:32 +0530

    IconChanges

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: 'web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: 'web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
